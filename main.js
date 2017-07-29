2ad3f804e1480956eb88b919ded684dd0f74e164
unknown <ghosh_su@Subhadip-G.India.XoriantCorp.com>
2017-07-29 13:42:48 +0530

    ok

--- main.js
+++ main.js
@@ -11,4 +11,10 @@
 
 		};
 
+		$scope.cancel=function(){
+			user.name="";
+			$scope.valueSel=0;
+		}
+		$scope.vals=[{val:0,name:"select"},{val:1,name:"aa"},{val:2,name:"bb"}];
+		$scope.valueSel=1;
 	});
